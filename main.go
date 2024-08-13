25772c5784a83b3301597f01579e3ba80433300c
Chris Wren <chris.wren@bt.com>
2024-08-13 20:10:48 +0100

    Setup Gin Server

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	github.com/gin-gonic/gin"
+  "net/http"
+  "github.com/gin-gonic/gin"
 )
 
 func main()  {
+  server := gin.Default()
 
+  server.GET("/events", getEvents)
+  
+  server.Run(":8080")
+}
+
+func getEvents(context *gin.Context){
+  context.JSON(http.StatusOK, gin.H{"message":"hello world"} )
 }
